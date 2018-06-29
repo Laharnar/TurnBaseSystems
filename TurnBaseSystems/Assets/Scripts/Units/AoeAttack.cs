d6560b963999be4ab24469af073990a7f274771c
Laharnar <marko.laharnar@gmail.com>
2018-06-29 12:15:37 +0200

    moved scripts

--- TurnBaseSystems/Assets/Scripts/Units/AoeAttack.cs
+++ TurnBaseSystems/Assets/Scripts/Units/AoeAttack.cs
@@ -1,3 +1,11 @@
-﻿public abstract class AoeAttack : Attack {
-    public GridMask aoeMask;
+﻿[System.Serializable]
+public class AOEAttack : Attack {
+
+    public int damage = 1;
+    public float range = 3f; // max 3 slots in 1 direction
+    public override void ApplyDamage(Unit source, GridItem attackedSlot) {
+        if (!requiresUnit || attackedSlot.filledBy) {
+            attackedSlot.filledBy.GetDamaged(damage);
+        }
+    }
 }
